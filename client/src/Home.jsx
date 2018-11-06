86975f5af25f95429c673cf5cece26252c93dcc5
Sarah Currey <curreysarah28@gmail.com>
2018-11-06 09:58:34 -0800

    form component created

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Form from './Form';
 
 class Home extends Component {
   render() {
@@ -13,58 +14,7 @@ class Home extends Component {
             <li className="login">Login</li>
           </ul>
         </nav>
-      <div className="full-form">
-        <h2 className="book"> Book Unique Homes and Experiences. </h2>
-        <form>
-          <section className="where">
-              <label>
-                WHERE
-              </label>
-                 <br/>
-                  <input
-                      type= "text"
-                      placeholder= "New York, NY, United States"
-                      name="where"
-                  />
-           </section>
-          <section className="check-in">
-          <label>
-            CHECK IN
-          </label>
-            <br/>
-              <input
-                  type= "text"
-                  placeholder= "mm/dd/yyyy"
-                  name="check-in"
-              />
-          </section>
-          <section className="check-out">
-          <label>
-            CHECK OUT
-          </label>
-            <br/>
-              <input
-                  type= "text"
-                  placeholder= "mm/dd/yyyy"
-                  name="check-out"
-              />
-          </section>
-          <section className="guests">
-          <label>
-            GUESTS
-          </label>
-             <br/>
-              <input
-                  type= "text"
-                  placeholder= "1 guest"
-                  name="guest"
-              />
-          </section>
-          <section className="search">
-          <input className="search-button" type="submit" value="Search" />
-          </section>
-        </form>
-        </div>
+         <Form/>
       </div>
       )
   }
