9e7585b90f5c67bd6d05d3cd8bb51708dd3f5b25
Ryufire22270 <mjacobs21321@gmail.com>
2018-11-06 10:59:36 -0500

    Matt, added db schema, seeds, and db info

---
+++ db/airbnb-database/airbnb.sql
@@ -0,0 +1,75 @@
+INSERT INTO listings (url, listing_title, city_location, room_specifics, superhost_or_not, description, contact_host, ammenities, sleep_arrange, access, availability, reviews, price) 
+(
+	"https://a0.muscache.com/im/pictures/8b20491f-d0c3-4fc2-bc9c-048a0ee78ef8.jpg?aki_policy=xx_large",
+	"Tasteful Room in Charming Two Bedroom Apartment",
+	"New York, NY",
+	"1 guest, 1 bedroom, 1 bed, 1 shared bath",
+	"Yoni is a Superhost: Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests."
+	"A comfortable and quiet room with a single bed and furnishings is available in this warm two bedroom in historic Harlem neighborhood. With great morning light, a friendly poodle, and French press coffee, it is down the block from the express subway/metro station, only 15 minutes away from the city center. Senior friendly. A great launching pad to explore the city!
+	
+	The space
+	Mid-century modern and rustic pieces decorate the space alongside plenty of typographic and illustrative art. Hardwood floors bathed with great morning light, the apartment is quiet and relaxing—a perfect place to come home to after an exhausting day running around the massive and fast-paced city.
+	A ground level apartment in the back of the building, it is a quiet space that can be ideal for all travelers including seniors. The bathroom has bars in the shower for secured support. Generous closet space and storage for your luggage is available in your room.
+	
+	Guest access
+	Check-in and Check-out times are very flexible based on your needs. We don't book overlapping reservations so you will never feel pressured to arrive or leave by a certain time of day. You will have full access to the kitchen, wifi, and an electronic key so you may come and go as you please.
+
+	Interaction with guests
+	We work full time during the day and will completely respect your privacy. Always open to chat and offer tips on conquering the city. In the evenings during the week we spend a lot of our time next door cooking and watching TV with the dog so you will have plenty of privacy. Weekends we tend to relax a bit in the living room and read or go to the park with Oreo.
+
+	Other things to note
+	There is a laundry room in the basement of the building. We recycle. Oreo is usually very quiet, but sometimes he can bark. We have extra towels, pillows, blankets and umbrellas. You are free to spend as much time in the home as you like watching TV or reading books.",
+	"Contact Host",
+	"Kitchen, Dryer, Wifi, Iron, Washer, Laptop friendly workspace",
+	"1 Bedroom Single Bed",
+	"Rosa Valeska
+	October 2018
+
+	Incredibly convenient, clean, and Yoni was absolutely awesome. There is a subway station at the end of the block, there are places to eat within walking distance, and I felt safe in the neighborhood. Yoni's place is very clean and he offered everything that's listed. This was my first solo trip and having Yoni's insights into the city and comfortable living space made me feel at ease. It was easy check in and check out. I highly recommend Yoni's place if you're traveling to New York.
+
+	Gabriel
+	October 2018
+
+	I enjoyed staying at Yoni's place. He was very responsive, quick to answer my questions. The neighborhood is nice and quiet and his apartment is very quiet for Manhattan. There are a lot of great restaurants and attractions in the neighborhood. A subway stop is very close by. Literally, at the end of the block you'll find a stop with two major lines. Yoni is kind and gave me all the space I need. His dog, Oreo, is adorable. I recommend.",
+	"60$ per night"
+)
+
+
+INSERT INTO host_info (title, city_location, date_joined, superhost_or_not, description, contact_host, policies, cancellation) VALUES
+(
+	"Hosted by Yoni",
+	"New York, New York, United States",
+	"Joined in May 2013",
+	"Yoni is a Superhost · Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.",
+	"Native New Yorker living in Harlem with my partner of 5 years and our dog, a miniature poodle, Oreo. That's me on the right and Matthew on the left.
+	Languages: English, Español, עברית
+	Response rate: 100%
+	Response time: within an hour",
+	"Contact Host",
+	"Policies
+	House Rules
+	Not suitable for children and infants
+	No smoking
+	No pets
+	No parties or events
+	Check-in time is flexible
+	Check out by 12PM (noon)
+	More information
+	Please don't bring back any strangers to the apartment. If you have a friend you'd like to invite over for a bit, please ask first. LGBTQ and dog friendly are nice.
+
+	Please take your shoes off when entering the apartment.
+
+	Please ask about recycling, laundry, storage, and anything else you may be curious about.
+
+	Don't let the front door slam. It is very heavy and loud.
+
+	Please do not smoke in the apartment.
+
+	If you are the only one in the apartment when you leave, please make sure the lights, fans, and air conditioner are turned off.
+
+	You must also acknowledge
+	Pet(s) live on property",
+	"Cancellations
+	Strict - Free cancellation for 48 hours
+	After that, cancel up to 7 days before check-in and get a 50% refund, minus the service fee."
+)
