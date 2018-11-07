f63eeae59d729be4993fcc0e12ea25ec4f2732a0
Ryufire22270 <mjacobs21321@gmail.com>
2018-11-07 10:51:47 -0500

    'changed image of first listing'

--- db/airbnb-database/airbnb.sql
+++ db/airbnb-database/airbnb.sql
@@ -1,16 +1,16 @@
 INSERT INTO listings (url, listing_title, city_location, room_specifics, superhost_or_not, description, contact_host, amenities, sleep_arrange, access, reviews, price) VALUES
 (
-	'http://the-window.info/wp-content/uploads/2017/03/building-design-wikipedia-beautiful-building-designs.jpg',
+	'https://upload.wikimedia.org/wikipedia/commons/9/96/Vasskertentrance.jpg',
 	'Tasteful Room in Charming Two Bedroom Apartment',
 	'New York, NY',
 	'1 guest, 1 bedroom, 1 bed, 1 shared bath',
 	'Yoni is a Superhost: Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.',
 	'A comfortable and quiet room with a single bed and furnishings is available in this warm two bedroom in historic Harlem neighborhood. With great morning light, a friendly poodle, and French press coffee, it is down the block from the express subway/metro station, only 15 minutes away from the city center. Senior friendly. A great launching pad to explore the city!
-	
+
 	The space
 	Mid-century modern and rustic pieces decorate the space alongside plenty of typographic and illustrative art. Hardwood floors bathed with great morning light, the apartment is quiet and relaxing—a perfect place to come home to after an exhausting day running around the massive and fast-paced city.
 	A ground level apartment in the back of the building, it is a quiet space that can be ideal for all travelers including seniors. The bathroom has bars in the shower for secured support. Generous closet space and storage for your luggage is available in your room.
-	
+
 	Guest access
 	Check-in and Check-out times are very flexible based on your needs. We do not book overlapping reservations so you will never feel pressured to arrive or leave by a certain time of day. You will have full access to the kitchen, wifi, and an electronic key so you may come and go as you please.
 
