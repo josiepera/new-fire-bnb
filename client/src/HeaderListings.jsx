9d2f5679210c6c6edd0dce0bb6d2e8386ef02475
Sarah Currey <curreysarah28@gmail.com>
2018-11-08 14:54:38 -0500

    nav changes to HeaderListings

--- client/src/HeaderListings.jsx
+++ client/src/HeaderListings.jsx
@@ -19,36 +19,38 @@ import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap
   render() {
     return (
       <React.Fragment>
-     <img src="https://i.imgur.com/4PAHoNj.png" title="source: imgur.com" />
-     <section className="dropdown menu">
-      <Dropdown isOpen={this.state.dropdownOpen} toggle={() =>this.toggle()}>
-        <DropdownToggle caret>
-          ^
-        </DropdownToggle>
-        <DropdownMenu>
-          <DropdownItem>Home</DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem>Invite Friends</DropdownItem>
-          <DropdownItem>Refer Hosts</DropdownItem>
-          <DropdownItem>Airbnb for Work</DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem>Host a Home</DropdownItem>
-          <DropdownItem>Sign Up</DropdownItem>
-          <DropdownItem>Login</DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem>Help</DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-      <form>
-        <label>
-          <div className="search-icon">
-              <svg className="search-picture">
-                  <path d="m2.5 7c0-2.5 2-4.5 4.5-4.5s4.5 2 4.5 4.5-2 4.5-4.5 4.5-4.5-2-4.5-4.5m13.1 6.9-2.8-2.9c.7-1.1 1.2-2.5 1.2-4 0-3.9-3.1-7-7-7s-7 3.1-7 7 3.1 7 7 7c1.5 0 2.9-.5 4-1.2l2.9 2.8c.2.3.5.4.9.4.3 0 .6-.1.8-.4.5-.5.5-1.2 0-1.7"></path></svg>
-               <input type="text"  name="query" placeholder="Search" value="New York, NY"/>
-          </div>
-        </label>
-      </form>
-     </section>
+        <div className="header-nav">
+           <img className="head-logo" src="https://i.imgur.com/4PAHoNj.png" title="source: imgur.com" />
+           <section className="dropdown menu">
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={() =>this.toggle()}>
+              <DropdownToggle className="caret" caret>
+                ^
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem>Home</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Invite Friends</DropdownItem>
+                <DropdownItem>Refer Hosts</DropdownItem>
+                <DropdownItem>Airbnb for Work</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Host a Home</DropdownItem>
+                <DropdownItem>Sign Up</DropdownItem>
+                <DropdownItem>Login</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Help</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+            <form>
+              <label>
+                <div className="search-icon">
+                    <svg className="search-picture">
+                        <path d="m2.5 7c0-2.5 2-4.5 4.5-4.5s4.5 2 4.5 4.5-2 4.5-4.5 4.5-4.5-2-4.5-4.5m13.1 6.9-2.8-2.9c.7-1.1 1.2-2.5 1.2-4 0-3.9-3.1-7-7-7s-7 3.1-7 7 3.1 7 7 7c1.5 0 2.9-.5 4-1.2l2.9 2.8c.2.3.5.4.9.4.3 0 .6-.1.8-.4.5-.5.5-1.2 0-1.7"></path></svg>
+                     <input className="search-input" type="text"  name="query" placeholder="Search" value="New York, NY"/>
+                </div>
+              </label>
+            </form>
+           </section>
+        </div>
      </React.Fragment>
     );
   }
