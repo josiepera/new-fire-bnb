3b896a2e1576c6fcb1be35c2ca011cad54faa66f
dillonbartkus <dillonbartkus@gmail.com>
2018-11-08 22:12:41 -0500

    listings styles

--- client/src/Listings.jsx
+++ client/src/Listings.jsx
@@ -1,36 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 
 const Listings = (props) => {
   return (
+
     <div className="listings">
-     <section className="filters">
-         <ul>
-            <li className="filter-buttons">Dates</li>
-            <li className="filter-buttons">Guests</li>
-            <li className="filter-buttons">Home type</li>
-            <li className="filter-buttons">Price</li>
-         </ul>
-     </section>
-     <section>
-     <img src="https://i.imgur.com/3Bdhpmu.png" title="source: imgur.com" />
-     <h1 className="threehundredhomes"> +300 homes</h1>
-     </section>
-     <section>
-      <img src={props.listings.url} />
-      <h2>{props.listings_title}</h2>
-      <p>{props.listings.room_specifics}</p>
-      <Link to={`/listings/${props.listings.id}`}>See More</Link>
 
-      <section className="price-info">
-      <h5>{props.listings.price}</h5>
-      <p>{props.listings.superhost_or_not} </p>
+      <div className="pics">
+      <img className="mainpic" src={props.listings.url} />
+      <img className="secpic" src={props.listings.url_two} />
+      <img className="thirdpic" src={props.listings.url_three} />
+      </div>
 
-      </section>
+      <div className="info">
+      <h2 className="title">{props.listings.listing_title}</h2>
+      <p className="details">🏠 {props.listings.room_specifics}</p>
+      <h5 className="price">💵 {props.listings.price}</h5>
+      <p className="super">🌟 {props.listings.superhost_or_not} </p>
+      <Link to={`/listings/${props.listings.id}`}>👀 See More</Link>
+
+      </div>
+
     </div>
   )
 }
+
+
 
 export default Listings;
