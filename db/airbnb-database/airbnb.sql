76bcdb9702adbad121e0d6cf0bfa231e575b4715
Ryufire22270 <mjacobs21321@gmail.com>
2018-11-09 10:03:45 -0500

    comma added

--- db/airbnb-database/airbnb.sql
+++ db/airbnb-database/airbnb.sql
@@ -74,7 +74,7 @@ INSERT INTO host_info (host_title, host_photo, host_city_location, date_joined, 
 	'Cancellations
 	Strict - Free cancellation for 48 hours
 	After that, cancel up to 7 days before check-in and get a 50% refund, minus the service fee.'
-)
+),
 (
 	'Hosted by Maria',
 	'https://upload.wikimedia.org/wikipedia/commons/6/6a/Gal_Gadot_2018_cropped_retouched.jpg',
