265f7ef1c87862a721a4ff4a9d5684fe4b448b3d
Sarah Currey <curreysarah28@gmail.com>
2018-11-10 14:30:43 -0500

    changes to home

--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -1,49 +1,11 @@
 import React, { Component } from 'react';
 
-import axios from 'axios';
-
-import { Redirect } from 'react-router-dom';
-
-
 class Form extends Component {
-  state = {
-    cityLocation: '',
-    fireRedirect: false,
-  };
-
-  handleInputChange(e) {
-    const name = e.target.name;
-    const value = e.target.value;
-    console.log(value)
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  handleFormSubmit(e) {
-    e.preventDefault();
-    axios
-      .get('/listings', {
-        cityLocation: this.state.city_location
-      })
-      .then(res => {
-        console.log(res);
-        this.setState({
-          newId: res.data.data.id,
-          fireRedirect: true,
-        })
-      })
-      .catch(err => console.log(err));
-      e.target.reset();
-  }
-
-
-
   render () {
     return(
        <div className="full-form">
         <h2 className="book"> Book Unique Homes and Experiences. </h2>
-        <form className="home-form" onSubmit={(e) => this.handleFormSubmit(e)}>
+        <form>
           <section className="where">
               <label>
                 WHERE
@@ -53,51 +15,46 @@ class Form extends Component {
                       type= "text"
                       placeholder= "New York, NY, United States"
                       name="where"
-                      value={this.state.city_location}
-                      onChange={(e) => this.handleInputChange(e)}
                   />
            </section>
-              <section className="check-in">
-              <label>
-                  CHECK IN
-              </label>
-                  <br/>
-                    <input
-                      type= "text"
-                      placeholder= "mm/dd/yyyy"
-                      name="check-in"
-                  />
-              </section>
-              <section className="check-out">
-              <label>
-                CHECK OUT
-              </label>
-                <br/>
-                  <input
-                      type= "text"
-                      placeholder= "mm/dd/yyyy"
-                      name="check-out"
-                  />
-              </section>
+          <section className="check-in">
+          <label>
+            CHECK IN
+          </label>
+            <br/>
+              <input
+                  type= "text"
+                  placeholder= "mm/dd/yyyy"
+                  name="check-in"
+              />
+
+          </section>
+          <section className="check-out">
+          <label>
+            CHECK OUT
+          </label>
+            <br/>
+              <input
+                  type= "text"
+                  placeholder= "mm/dd/yyyy"
+                  name="check-out"
+              />
+          </section>
           <section className="guests">
           <label>
             GUESTS
           </label>
              <br/>
-              <select className="guests">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-              </select>
+              <input
+                  type= "text"
+                  placeholder= "1 guest"
+                  name="guest"
+              />
           </section>
           <section className="search">
           <input className="search-button" type="submit" value="Search" />
           </section>
         </form>
-         {this.state.fireRedirect
-          ? <Redirect push to={`/listings/`} />
-          : ''}
         </div>
       )
   }
